aa01c827f8ff488290e179b582f5d2640f16110f
Leonardo <ljcl79@gmail.com>
2020-12-13 13:29:26 -0300

    Aula1

---
+++ Cuenta.js
@@ -0,0 +1,34 @@
+export class Cuenta {
+    #cliente;
+    #saldo;
+
+    constructor(cliente, numero, agencia, saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.#cliente = cliente;
+        this.#saldo = saldo;
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor,cuentaDestino) {
+        this.retirarDeCuenta(valor);
+        cuentaDestino.depositoEnCuenta(valor);
+        valor = 200;
+        valor = valor*1000;
+    }
+}
--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
+//import {CuentaCorriente} from './CuentaCorriente.js';
+//import {CuentaAhorro} from './CuentaAhorro.js';
+import {Cuenta} from './Cuenta.js'
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const cuentaDeLeonardo = new Cuenta(cliente, '1', '001',0);
+const cuentaDeMaria = new Cuenta(cliente2,'2','002',0);
 
-let saldo = cuentaDeLeonardo.verSaldo(); 
+const cuentaAhorroLeonardo = new Cuenta(cliente,'9985','001',0);
 
-saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
-
-const saldoMaria = cuentaDeMaria.verSaldo();
-
-console.log('Cuenta de Maria',cuentaDeMaria);
-
-console.log('El Saldo actual (cuentaMaria) '+saldoMaria);
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
-console.log(CuentaCorriente.cantidadCuentas);
-
-console.log(CuentaCorriente.cantidadCuentas);
-
-
-
-
+console.log(cuentaDeLeonardo);
+cuentaDeLeonardo.depositoEnCuenta(150);
+console.log(cuentaDeLeonardo.verSaldo());
+cuentaDeLeonardo.retirarDeCuenta(70);
+console.log(cuentaDeLeonardo.verSaldo());
+console.log(cuentaAhorroLeonardo);
+cuentaAhorroLeonardo.depositoEnCuenta(200);
+console.log(cuentaAhorroLeonardo.verSaldo());
+cuentaAhorroLeonardo.retirarDeCuenta(120);
+console.log(cuentaAhorroLeonardo.verSaldo());
